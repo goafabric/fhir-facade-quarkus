4d2a189c98f1b341c68bfb3fa1454cf482e44cca
Andreas Mautsch <amautsch>
2021-07-14 08:19:21 +0200

    bundleservice added

---
+++ src/main/java/org/goafabric/fhir/logic/BundleLogic.java
@@ -0,0 +1,48 @@
+package org.goafabric.fhir.logic;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.Resource;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+@Slf4j
+public class BundleLogic {
+    @Inject
+    PatientLogic patientLogic;
+
+    @Inject
+    PractitionerLogic practitionerLogic;
+
+    public Bundle getBundle(IdType idType) {
+        if (!"1".equals(idType.getIdPart())) {
+            throw new ResourceNotFoundException("bundle not found");
+        }
+
+        final Bundle bundle = new Bundle();
+        bundle.addEntry(createBundleEntry(
+                patientLogic.getPatient(idType)));
+        bundle.addEntry(createBundleEntry(
+                practitionerLogic.getPractitioner(idType)));
+
+        return bundle;
+    }
+
+    private Bundle.BundleEntryComponent createBundleEntry(Resource resource) {
+        final Bundle.BundleEntryComponent bundleEntry = new Bundle.BundleEntryComponent();
+        bundleEntry.setResource(resource);
+        bundleEntry.setFullUrl(resource.getClass().getSimpleName() + "/" + resource.getId());
+        return bundleEntry;
+    }
+
+    public MethodOutcome createBundle(Bundle bundle) {
+        log.info(FhirContext.forR4().newJsonParser().encodeResourceToString(bundle));
+        return new MethodOutcome();
+    }
+}
