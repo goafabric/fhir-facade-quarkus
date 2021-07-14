fb479d4ff7b146aedb592f3310ca4c4433252d33
Andreas Mautsch <amautsch>
2021-07-14 09:15:31 +0200

    changes

--- src/main/java/org/goafabric/fhir/configuration/ReflectionConfigurationR4Full.java
+++ src/main/java/org/goafabric/fhir/configuration/ReflectionConfigurationR4Full.java
@@ -7,15 +7,18 @@ import org.hl7.fhir.r4.model.*;
         //Base classes
         ca.uhn.fhir.context.FhirVersionEnum.class,
         org.hl7.fhir.utilities.xhtml.XhtmlNode.class,
+        ca.uhn.fhir.rest.api.server.IFhirVersionServer.class,
+
+        //Service Classes needed for Metadata
+        org.goafabric.fhir.service.PatientService.class,
+        org.goafabric.fhir.service.PractitionerService.class,
+        org.goafabric.fhir.service.BundleService.class,
 
         //Parameter classes
         ca.uhn.fhir.rest.param.StringParam.class,
 
         //R4 Context and Service classes
         org.hl7.fhir.r4.hapi.ctx.FhirR4.class,
-        org.goafabric.fhir.service.PatientService.class,
-        org.goafabric.fhir.service.PractitionerService.class,
-        org.goafabric.fhir.service.BundleService.class,
 
         //R4 model classes
         Account.class,
