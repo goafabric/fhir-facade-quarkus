27bc745003bd5e596b3b640ad0b43be0d926b78b
Andreas Mautsch <amautsch>
2021-07-14 13:59:42 +0200

    client added

--- src/main/java/org/goafabric/fhir/adapter/mock/PersonServiceMockAdapter.java
+++ src/main/java/org/goafabric/fhir/adapter/mock/PersonServiceMockAdapter.java
@@ -4,11 +4,10 @@ import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import org.goafabric.fhir.adapter.Person;
 import org.goafabric.fhir.adapter.PersonServiceAdapter;
 
-import javax.enterprise.context.ApplicationScoped;
 import java.util.Arrays;
 import java.util.List;
 
-@ApplicationScoped
+//@ApplicationScoped
 public class PersonServiceMockAdapter implements PersonServiceAdapter {
     @Override
     public Person getById(String id) {
--- src/main/java/org/goafabric/fhir/adapter/remote/PersonServiceRemoteAdapter.java
+++ src/main/java/org/goafabric/fhir/adapter/remote/PersonServiceRemoteAdapter.java
@@ -4,15 +4,11 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.goafabric.fhir.adapter.Person;
 import org.goafabric.fhir.adapter.PersonServiceAdapter;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Path("/persons")
-@Produces(MediaType.APPLICATION_JSON)
-//@ApplicationScoped
+@ApplicationScoped
 public class PersonServiceRemoteAdapter implements PersonServiceAdapter {
     @Inject
     @RestClient
