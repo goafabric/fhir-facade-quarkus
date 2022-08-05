494e5f65e95c5e6a09d74ad898d9e6a11c4c4c7c
Andreas Mautsch <amautsch>
2022-08-05 08:01:46 +0200

    release version

--- build-native.sh
+++ build-native.sh
@@ -2,6 +2,6 @@
 git pull
 time mvn -B clean install -P docker-image-native
 
-docker pull goafabric/fhir-facade-quarkus:2.0.0-SNAPSHOT && docker run --name fhir-facade-quarkus --rm -p50700:50700 goafabric/fhir-facade-quarkus:2.0.0-SNAPSHOT -Xm32m
+docker pull goafabric/fhir-facade-quarkus:2.0.0 && docker run --name fhir-facade-quarkus --rm -p50700:50700 goafabric/fhir-facade-quarkus:2.0.0 -Xm32m
 
 
